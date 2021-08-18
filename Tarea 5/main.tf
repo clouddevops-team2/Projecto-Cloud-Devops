acf39e8a460d3779e666b203a90bcc0f29331dae
fedebertazzo <fedebertazzo@gmail.com>
2021-08-18 20:36:24 -0300

    Carga de tareas 5 y 6

---
+++ Tarea 5/main.tf
@@ -0,0 +1,49 @@
+provider "aws" { #definir el proveedor cloud
+    region = "sa-east-1" #Definir la región a gestionar
+<<<<<<< HEAD:Tarea 5/Terraform_Lab/main.tf
+    access_key = "XXXXXXX"
+    secret_key = "XXXXXXX"
+=======
+    access_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx"
+    secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+>>>>>>> 40ab8d7ec71ae13590aa0e6e11cb07b6263965d0:Tarea 5/main.tf
+
+}
+
+resource "aws_instance" "terraform-test" { #Crear una instancia
+    ami = "ami-054a31f1b3bf90920" #Seleccionar la AMI correspondiente a la región (Ubuntu Server 20.04 LTS (HVM), SSD Volume Type)
+    instance_type = "t2.micro"
+    tags = {
+      "Name" = "terraform-test"
+      "mgmt" = "auto-managed"
+      "date" = "05-08-2021"
+    }    
+}
+
+resource "aws_security_group" "example_rules" { #Crear un grupo de seguridad
+    name = "security-group-terraform-tcp" #Nombre de la regla
+    description = "Permitir TCP-SSH"
+    ingress = [{
+        description = "SSH Access"
+        from_port = 60022 #SSH
+        to_port = 60022 #SSH
+        protocol = "tcp" #TCP
+        cidr_blocks = ["18.65.127.36/32"] #Se pueden agregar varias direcciones en lista ["18.65.127.36/32","18.65.137.11/32"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        security_groups = []
+        self = false
+
+    }]
+      egress = [{
+          description ="All traffic"
+          from_port= 0
+          to_port = 0
+          protocol = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+          prefix_list_ids = []
+          security_groups = []
+          self = false
+    }]
+}
---
+++ Tarea 6/Tarea 6.md
@@ -0,0 +1,13 @@
+# Tarea 6
+
+- [x]  Carlos realizara la siguiente tarea: Desplegar de manera manual la aplicación que el profesor suministro, en 1 servidor en AWS, utilizando EC2, Creando una Instancia de Windows Server 2019 t2.micro, que tenga instalado IIS o en su defecto, WAMP, para así disponer de un servidor Web, en el , simplemente copiaremos nuestro código en el web server, y visitaremos localhost para identificar que la aplicación se encuentra en la nube en una Instancia. La instancia debe ser creada con acceso RDP desde cualquier dirección IP , también debe tener un disco EBS de 30gb, con un disco de SSD de 3era Generación.
+- [x]  Tags que deben tener: Tipo: InstanciaWebServer / OS: Windows Server2019 / Equipo: CloudDevopsTeam2 / Carrera: CloudDevOps / Institución: EducacionIT / Environment: Pre-Production
+- [x]  Federico se encargara de actualizar el status de esta tarea en Trello y también en Github
+
+Capturas:
+
+![InstanciaWebServer.png](InstanciaWebServer.png)
+
+![InstanciaWebServer_security.png](InstanciaWebServer_security.png)
+
+![InstanciaWebServer_storage.png](InstanciaWebServer_storage.png)
